18ff31508330707a4bee21bdb00735c8ae40f165
CesarG282 <140753116+CesarG282@users.noreply.github.com>
2024-12-04 15:58:02 -0500

    INSERTO FORMULARIO DE SIGNUP
    FORMULARIO DE LOGIN

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return render(request, 'hello.html',{})
+def home(request):
+    return render(request, 'home.html')
+
+def helloworld(request):
+    return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
